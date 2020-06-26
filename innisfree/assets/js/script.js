2396de78fc0b5ca69a89f101ec39d7d624a2d9a5
moonsemi <48460923+moonsemi@users.noreply.github.com>
2020-06-26 23:27:05 +0900

    main-article04 almost done
    - main-article04 swiper api 수정완료
    - arrow button, autoplay play/stop 작업 중 커밋

--- innisfree/assets/js/script.js
+++ innisfree/assets/js/script.js
@@ -1,2 +1,51 @@
+$(document).ready(function(){
+    let mainVisualSwiper = new Swiper('.main-visual', {
+        loop : true,
+        speed: 1000,
+        autoplay: {
+            delay: 3000,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.mv-btn-next',
+            prevEl: '.mv-btn-prev',
+        }
+    });
+    // mainVisualSwiper js  
+    function autoplayPlay(){
+        mainVisualSwiper.autoplay.start();
+    }
+    function autoplayStop(){
+        mainVisualSwiper.autoplay.stop();
+    }
+    // mainVisualSwiper-autoplay play/stop js end;
+    
+    let saleRankingSwiper = new Swiper('.sale-ranking', {
+        slidesPerView: 4,
+        spaceBetween: 60,
+        loop : true,
+        speed: 1000,
+        // autoplay: {
+        //     delay: 3000,
+        // },
+        navigation: {
+            nextEl: '.sr-btn-next',
+            prevEl: '.sr-btn-prev',
+        }
+    });
+    // saleRankingSwiper js
 
+    function autoplayPlay(){
+        saleRankingSwiper.autoplay.start();
+    }
+    function autoplayStop(){
+        saleRankingSwiper.autoplay.stop();
+    }
+    // autoplay play/stop js
+    // saleRankingSwiper-autoplay play/stop js end;
+});
 
